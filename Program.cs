160d4fbf559cd23c34ae2aebd70b0813769d71b9
GDIfox <gidmitriy@me.com>
2023-11-09 01:13:25 +0300

    add: variant performance of tasks

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿string[] arr = { "Hello", "2", "world", ":-)" };
+
+int max_simvol = 3;
+
+string[] NewArray(string[] array)
+{
+    int length = array.Length;
+    string[] rez = new string[length];
+    int count = 0;
+
+    for (int i = 0; i < length; i++)
+    {
+        if (array[i].Length <= max_simvol)
+        {
+            rez[count] = array[i];
+            count++;
+        }
+    }
+    Array.Resize(ref rez, count);
+    return rez;
+}
+void PrintArray(string[] array)
+{
+    Console.Write("[ ");
+    for (int i = 0; i < array.Length; i++)
+    {
+        // var rezultat = "[" + String.Join(", ", arr) + "]";
+        // Console.WriteLine(rezultat);
+        Console.Write($"“{array[i]}”, ");
+    }
+    Console.Write("] ");
+}
+void Rezult(string[] array)
+{
+    PrintArray(array);
+    string[] Array = NewArray(array);
+
+    PrintArray(Array);
+    Console.WriteLine();
+}
+
+Rezult(arr);
